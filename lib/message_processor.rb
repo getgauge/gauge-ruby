ae069d0224d6b4cb398a5fd7d9221371c249e53d
sriv <srikanth.ddit@gmail.com>
2015-01-15 12:20:17 +0530

    added error response for refactor request

--- lib/message_processor.rb
+++ lib/message_processor.rb
@@ -102,12 +102,12 @@ def refactor_step(message)
   newStep = message.refactorRequest.newStepValue
   stepBlock = $steps_map[oldStepValue]
   refactor_response = Main::RefactorResponse.new(success: true)
-  # begin
+  begin
     Gauge::CodeParser.refactor stepBlock, message.refactorRequest.paramPositions, newStep.parameters, newStep.stepValue
-  # rescue Exception => e
-  #   refactor_response.success=false
-  #   refactor_response.error=e.message
-  # end
+  rescue Exception => e
+    refactor_response.success=false
+    refactor_response.error=e.message
+  end
   Main::Message.new(:messageType => Main::Message::MessageType::RefactorResponse, :messageId => message.messageId, refactorResponse: refactor_response)
 end
 
