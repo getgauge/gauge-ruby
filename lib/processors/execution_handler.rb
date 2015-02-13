7fa4b8b60afae30e456210c0748d25cdcf16911c
sriv <srikanth.ddit@gmail.com>
2015-02-13 11:24:36 +0530

    included check to see if gauge_screenshot is installed

--- lib/processors/execution_handler.rb
+++ lib/processors/execution_handler.rb
@@ -73,7 +73,19 @@ module Gauge
           end
         end
         return params
-      end  
+      end
+
+      private
+      def which(cmd)
+        exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+        ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+          exts.each { |ext|
+            exe = File.join(path, "#{cmd}#{ext}")
+            return exe if File.executable?(exe) && !File.directory?(exe)
+          }
+        end
+        return nil
+      end      
     end
   end
 end
