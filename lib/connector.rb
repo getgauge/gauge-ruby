3a70912a383f1f93ca1cf3cb9d7e5d79d9d0ef17
BugDiver <vinaysh@thoughtworks.com>
2018-01-11 11:54:43 +0530

    removing api socket, using regex to get ste_value getgauge/gauge-vscode#85 #47

--- lib/connector.rb
+++ lib/connector.rb
@@ -37,7 +37,6 @@ module Gauge
 
     def self.make_connections
       @@executionSocket = TCPSocket.open(HOST_NAME, Runtime.portFromEnvVariable(GAUGE_PORT_ENV))
-      @@apiSocket = TCPSocket.open(HOST_NAME, Runtime.portFromEnvVariable(API_PORT_ENV))
     end
 
     def self.message_length(socket)
@@ -45,29 +44,7 @@ module Gauge
     end
 
     def self.step_value text
-      stepValueRequest = Gauge::Messages::GetStepValueRequest.new(:stepText => text)
-      apiMessage = Gauge::Messages::APIMessage.new(:messageType => Gauge::Messages::APIMessage::APIMessageType::GetStepValueRequest, :stepValueRequest => stepValueRequest)
-      response = get_api_response(apiMessage)
-      if (response.messageType == Gauge::Messages::APIMessage::APIMessageType::ErrorResponse)
-        puts "[Error] Failed to load step implementation. #{response.error.error}:  \"#{text}\""
-        return ''
-      end
-      return response.stepValueResponse.stepValue.stepValue
-    end
-
-    def self.get_api_response(apiMessage)
-      apiMessage.messageId = get_unique_id
-      dataLen = apiMessage.serialize_to_string.bytesize
-      ProtocolBuffers::Varint.encode @@apiSocket, dataLen
-      apiMessage.serialize(@@apiSocket)
-
-      responseLen = message_length(@@apiSocket)
-      data = @@apiSocket.read responseLen
-      message = Gauge::Messages::APIMessage.parse(data)
-    end
-
-    def self.get_unique_id
-      rand(2**63-1)
+      return text.gsub(/(<.*?>)/, "{}")
     end
   end
 end
