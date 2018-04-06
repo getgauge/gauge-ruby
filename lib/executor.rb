691a73240944d1c37f575c63741a6cd49359efad
BugDiver <vinaysh@thoughtworks.com>
2018-04-06 14:44:53 +0530

    fixed tags aggregation. removed old messages.

--- lib/executor.rb
+++ lib/executor.rb
@@ -17,7 +17,7 @@
 require 'ruby-debug-ide'
 require_relative 'gauge'
 
-ATTACH_DEBUGGER_EVENT = "Runner Ready for Debugging"
+ATTACH_DEBUGGER_EVENT = 'Runner Ready for Debugging'
 
 module Gauge
   class DebugOptions
@@ -42,7 +42,7 @@ module Gauge
       if ENV['DEBUGGING']
         options = DebugOptions.new
         options.host = '127.0.0.1'
-        options.port = ENV["DEBUG_PORT"].to_i
+        options.port = ENV['DEBUG_PORT'].to_i
         options.notify_dispatcher = false
         Gauge::Log.info ATTACH_DEBUGGER_EVENT
         Debugger.prepare_debugger(options)
@@ -59,22 +59,24 @@ module Gauge
     end
 
     def self.execute_hooks(hooks, currentExecutionInfo, should_filter)
-      begin
-        hooks.each do |hook|
-          if !should_filter || hook[:options][:tags].length == 0
-            next hook[:block].call(currentExecutionInfo)
-          end
-          tags = currentExecutionInfo.currentSpec.tags + currentExecutionInfo.currentScenario.tags
-          intersection = (tags & hook[:options][:tags])
-          if (hook[:options][:operator] == 'OR' && intersection.length > 0) ||
-              (hook[:options][:operator] == 'AND' && intersection.length == hook[:options][:tags].length)
-            hook[:block].call(currentExecutionInfo)
-          end
+      hooks.each do |hook|
+        if !should_filter || hook[:options][:tags].empty?
+          next hook[:block].call(currentExecutionInfo)
         end
-        return nil
-      rescue Exception => e
-        return e
+        tags = currentExecutionInfo.currentSpec.tags + get_scenario_tags(currentExecutionInfo)
+        intersection = (tags & hook[:options][:tags])
+        if (hook[:options][:operator] == 'OR' && !intersection.empty?) ||
+           (hook[:options][:operator] == 'AND' && intersection.length == hook[:options][:tags].length)
+          hook[:block].call(currentExecutionInfo)
+        end
       end
+      nil
+    rescue Exception => e
+      e
+    end
+
+    def self.get_scenario_tags(info)
+      !info.currentScenario.nil? ? info.currentScenario.tags : []
     end
   end
 end
