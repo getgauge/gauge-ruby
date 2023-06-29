bdf33e84acee95baa11049968010c01275cf41d9
Chad Wilson <chadw@thoughtworks.com>
2023-06-29 21:26:38 +0800

    Remove unnecessary script

--- genproto.bat
+++
@@ -1,7 +0,0 @@
-:: Copyright (c) ThoughtWorks, Inc.
-:: Licensed under the Apache License, Version 2.0
-:: See LICENSE.txt in the project root for license information.
-
-:: using gem ruby-protocol-buffers
-cd gauge-proto
-ruby-protoc -o ..\lib spec.proto && ruby-protoc -o ..\lib messages.proto && ruby-protoc -o ..\lib api.proto && cd ..
